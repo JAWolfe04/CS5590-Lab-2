b69c7594f0473396dcf025b65484491c16a75893
Jonathan <jawhf4@mail.umkc.edu>
2020-04-28 23:04:26 -0500

    Added commenting

--- Part_4/Source Code/part4/src/main/java/lab2/part4/App.java
+++ Part_4/Source Code/part4/src/main/java/lab2/part4/App.java
@@ -14,8 +14,10 @@ public class App
 {
     public static void main( String[] args )
     {
+    	// Get Spark Session
     	SparkSession spark = SparkSession.builder().appName("graphing").master("local[*]").getOrCreate();
     	
+    	// Make Schemas to implement in the creation of dataframes
     	StructType edgeSchema = new StructType(new StructField[] {
     			new StructField("id", DataTypes.IntegerType, false, Metadata.empty()),
     			new StructField("group1", DataTypes.IntegerType, false, Metadata.empty()),
@@ -33,30 +35,37 @@ public class App
     			new StructField("group_urlname", DataTypes.StringType, false, Metadata.empty())
     	});
     	
+    	// Pull edges and vertices from csv files with supplied schema
     	Dataset<Row> edgeDF = spark.read().option("header", "true").schema(edgeSchema)
     			.csv("C:\\Users\\Jonathan\\Desktop\\UMKC\\CS 5590\\Labs\\CS5590-Lab-2\\Part_4\\Input\\group-edges.csv");
     	Dataset<Row> vertexDF = spark.read().option("header", "true").schema(vertexSchema)
     			.csv("C:\\Users\\Jonathan\\Desktop\\UMKC\\CS 5590\\Labs\\CS5590-Lab-2\\Part_4\\Input\\meta-groups.csv");
     	
+    	// We only need to know the connections and a few attributes of the groups for interpretation
     	edgeDF = edgeDF.select("group1", "group2");
     	vertexDF = vertexDF.select("group_id", "group_name", "num_members");
     	
+    	// Need to supply the names as expected by GraphX
     	edgeDF = edgeDF.withColumnRenamed("group1", "src");
     	edgeDF = edgeDF.withColumnRenamed("group2", "dst");
     	vertexDF = vertexDF.withColumnRenamed("group_id", "id");
     	
+    	// Need to add a 2 way connection between groups with shared members
     	Dataset<Row> switchedEdges = edgeDF.withColumnRenamed("src", "dst1");
     	switchedEdges = switchedEdges.withColumnRenamed("dst", "src");
     	switchedEdges = switchedEdges.withColumnRenamed("dst1", "dst");
     	edgeDF.union(switchedEdges);
     	
+    	// Remove duplicate rows
     	vertexDF = vertexDF.distinct();
     	edgeDF = edgeDF.distinct();
     	
     	GraphFrame graph = new GraphFrame(vertexDF, edgeDF);
     	
+    	// Using more than 3 max iterations takes a long time to run
     	GraphFrame result = graph.pageRank().resetProbability(0.15).maxIter(3).run();
     	
+    	// Sort by pagerank to easily see the most influential groups to the least
     	result.vertices().sort(desc("pagerank")).coalesce(1).write().option("header", "true")
 		.csv("C:\\Users\\Jonathan\\Desktop\\UMKC\\CS 5590\\Labs\\CS5590-Lab-2\\Part_4\\Output\\PageRank.csv");
     }
